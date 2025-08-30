88a3412816da001f4ed454a2064501f4acd97c7d
Philip Yi <philip.yunfan.yi@gmail.com>
2025-08-30 13:47:30 -0400

    skeleton

--- Learn-Metal/main.cpp
+++ Learn-Metal/main.cpp
@@ -4,6 +4,13 @@
 //
 //  Created by Philip Yi on 8/30/25.
 //
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
 
 #include <iostream>
 
